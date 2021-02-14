6c17977549e463edc99d0cb50c7844eb60cce957
Arthur Bousquet <bousquet@lakeforest.edu>
2021-02-14 13:36:28 -0600

    changed the setup: added requirements

--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="pyeafe",
-    version="0.1",
+    version="0.1.1",
     description="Edge-Averaged Finite Elements (EAFE) for FENiCS",
     url="https://github.com/thepnpsolver/pyeafe",
     author="Maximilian Metti, Shuonan Wu, Arthur Bousquet",
-    author_email="pyeafe.team@gmail.com",
+    author_email="bousquet@lakeforest.edu",
     license="GNU Lesser General Public License",
     packages=["pyeafe"],
     zip_safe=False,
+    install_requires=[
+        "fenics>=2019.1.0",
+        "fenics-dolfin>=2019.1.0",
+        "numpy>=1.19.0",
+        "petsc4py>=3.12.0",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    tests_require=["pytest", "flake8", "black"],
+    setup_requires=["pytest-runner"],
 )
