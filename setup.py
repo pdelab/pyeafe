0b02a0d58cd21dc856385e08eee5dc7f6fac613a
Arthur Bousquet <arthbous@users.noreply.github.com>
2021-02-16 14:25:36 -0600

    change author to pnp solver (#11)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     version="0.1.1",
     description="Edge-Averaged Finite Elements (EAFE) for FENiCS",
     url="https://github.com/thepnpsolver/pyeafe",
-    author="Maximilian Metti, Shuonan Wu, Arthur Bousquet",
+    author="The PNP Solver",
     author_email="bousquet@lakeforest.edu",
     license="GNU Lesser General Public License",
     packages=["pyeafe"],
