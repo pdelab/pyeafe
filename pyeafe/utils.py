d4901e3d045348762604349cc7c2e0d40572968b
Maximilian Metti <maximilianmetti@gmail.com>
2021-03-02 21:31:37 -0500

    rename evaluate to utils and add a validator

--- pyeafe/utils.py
+++ pyeafe/utils.py
@@ -1,17 +1,76 @@
-from dolfin import Expression
 from inspect import getfullargspec
-from typing import Union
+from typing import Callable, Union
 
 import numpy as np
+from dolfin import (
+    Constant,
+    Expression,
+    Function,
+    # MultiMeshFunction,
+    Point,
+    Cell,
+)
 
-FlexibleExpression = Union[Expression, callable, np.array, float]
+# from dolfin.cpp.function import (
+#     Constant as CppConstant,
+#     Expression as CppExpression,
+#     Function as CppFunction,
+#     GenericFunction as CppGenericFunction,
+#     MultiMeshFunction as CppMultiMeshFunction,
+# )
+
+Coefficient = Union[
+    Constant,
+    Expression,
+    Function,
+    # MultiMeshFunction,
+    # CppConstant,
+    # CppExpression,
+    # CppFunction,
+    # CppGenericFunction,
+    # CppMultiMeshFunction,
+]
+
+
+def validate_coefficient(coefficient: Coefficient) -> bool:
+    if issubclass(coefficient.__class__, Constant):
+        return True
+
+    if issubclass(coefficient.__class__, Expression):
+        return True
+
+    if issubclass(coefficient.__class__, Function):
+        return True
+
+    # if issubclass(coefficient.__class__, MultiMeshFunction):
+    #     return True
+    #
+    # if issubclass(coefficient.__class__, CppConstant):
+    #     return True
+    #
+    # if issubclass(coefficient.__class__, CppExpression):
+    #     return True
+    #
+    # if issubclass(coefficient.__class__, CppFunction):
+    #     return True
+    #
+    # if issubclass(coefficient.__class__, CppGenericFunction):
+    #     return True
+    #
+    # if issubclass(coefficient.__class__, CppMultiMeshFunction):
+    #     return True
+
+    return False
 
 
 # TODO: pass in mesh and define `eval_cell`
-def create_safe_eval(expression: FlexibleExpression, value_shape: int) -> callable:
+def create_safe_eval(
+    expression: Coefficient,
+    value_shape: int,
+) -> Callable[[Point, Cell], Union[float, np.array]]:
     """
     Wrap expression to ensure consistent calling for various input coefficients.
-    :param expression: dolfin.Expression, python function, or constant value
+    :param expression: pyeafe.Coefficient to be evaluated
     :param value_shape: integer output dimension of function
 
     :return: Return a function that has a consistent parameter list:
