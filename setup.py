06d87f765da811a3db4f0098fee28b13b46f8da2
Arthur Bousquet <bousquet@lakeforest.edu>
2021-03-07 11:23:34 -0600

    fix steup

--- setup.py
+++ setup.py
@@ -15,8 +15,6 @@ setup(
     packages=["pyeafe"],
     zip_safe=False,
     install_requires=[
-        "fenics>=2019.1.0",
-        "fenics-dolfin>=2019.1.0",
         "numpy>=1.19.0",
         "petsc4py>=3.12.0",
         "typing>=3.7.4.3",
@@ -24,6 +22,5 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     tests_require=["pytest", "flake8", "black"],
-    setup_requires=["pytest-runner"],
     project_urls={"Source Code": "https://github.com/thepnpsolver/pyeafe"},
 )
